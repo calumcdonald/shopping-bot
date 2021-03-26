6c7a5043909ee9b881a088e1d4bfe465e9d1c399
Calum McDonald <calumcdonald@gmail.com>
2021-03-26 16:20:39 +0000

    Added more cards to choose from, prices for reference, webdriver bot should be good to go!

--- webdriver_bot.py
+++ webdriver_bot.py
@@ -15,7 +15,7 @@ with open("data/details.json") as f:
 with open("data/products.json") as f:
     products = json.load(f)
 
-product = products['phone_case']
+product = products['MSI_RTX3060_VENTUS_2X']
 
 CHROMEDRIVER_PATH = "chromedriver.exe"
 
@@ -27,98 +27,71 @@ options = Options()
 driver = webdriver.Chrome(CHROMEDRIVER_PATH, options=options)
 driver.get(product['url'])
 
-in_stock = False
-def restart():
-    in_stock = False
+checked_out = False
+def refresh():
     driver.delete_all_cookies()
     driver.get(product['url'])
 
-while not in_stock:
+def try_to_checkout():
     try:
-        # click off of cookie notice
-        #driver.find_element_by_xpath("//*[@id='onetrust-accept-btn-handler']").click()
-        WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='onetrust-accept-btn-handler']"))).click()
-        in_stock = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='product-actions']/div[4]/div[1]/button")))
-        # code won't get past here if it's out of stock
-        print('IN STOCK')
-        # add to basket
-        in_stock.click()
-        in_stock = True
-        # print("ADDED TO BASKET")
-        # let basket update
-        time.sleep(2)
-
         # straight to checkout
         driver.get("https://www.currys.co.uk/app/checkout")
         # enter post code
-        postcode = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='delivery_location']/input")))
-        postcode.send_keys(data["postcode"])
-        time.sleep(2)
-        postcode.send_keys(Keys.ENTER)
-        # print("POST CODE ENTERED")
+        try:
+            postcode = WebDriverWait(driver, 15).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='delivery_location']/input")))
+            postcode.send_keys(data["postcode"])
+            time.sleep(2)
+            postcode.send_keys(Keys.ENTER)
+        except:
+            pass
         # click free delivery
-        WebDriverWait(driver, 7).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(),'Free')]"))).click()
-        # print("DELIVERY SELECTED")
+        WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, "//button[contains(text(),'Free')]"))).click()
         # enter email
         email = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.NAME, "email")))
         email.send_keys(data["email"])
         WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//button[contains(text(),'Continue')]"))).click()
-        # print("EMAIL ENTERED")
         # continue as guest
         WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//a[contains(text(),'Continue as a guest')]"))).click()
-        # print("CONTINUING AS GUEST")
 
         # title
         WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.NAME, "d-title"))).click()
         WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//div[contains(text(),'Mr.')]"))).click()
-        # print("TITLE SELECTED")
         # first name
         fname = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.NAME, "fname")))
         fname.send_keys(data["firstname"])
-        # print("FIRST NAME ENTERED")
         # surname
         lname = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.NAME, "lname")))
         lname.send_keys(data["surname"])
-        # print("SURNAME ENTERED")
         # mobile number
         mobile = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='addresses']/div[2]/div[5]/div/input")))
         mobile.send_keys(data["mobile"])
-        # print("MOBILE ENTERED")
         # postcode
         postcode = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.NAME, "zip")))
         postcode.send_keys(Keys.CONTROL + "a")
         postcode.send_keys(data["postcode"])
-        # print("POSTCODE ENTERED")
         # address line 1
         address1 = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.NAME, "address")))
         address1.send_keys(data["address1"])
-        # print("ADDRESS LINE 1 ENTERED")
         # city
         city = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.NAME, "city")))
         city.send_keys(data["city"])
-        # print("CITY ENTERED")
         # submit
         city.send_keys(Keys.ENTER)
 
         # continue to enter card details
         WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, "//div[contains(text(),'Card')]"))).click()
-        # print("CARD PAYMENT SELECTED")
         # card num
         cardNum = WebDriverWait(driver, 10).until(EC.visibility_of_element_located((By.NAME, "cardNumber")))
         cardNum.send_keys(data["testcardnum"])
-        # print("CARD NUMBER ENTERED")
         # cardholder name
         cardName = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.NAME, "cardholderName")))
         cardName.send_keys(data["testcardholdername"])
-        # print("CARDHOLDER NAME ENTERED")
         # card exp month
         cardExpMon = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.NAME, "expiryDate.expiryMonth")))
         cardExpMon.send_keys(data["testexpmonth"])
-        # print("EXPIRY MONTH ENTERED")
         # card exp year
         cardExpYr = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.NAME, "expiryDate.expiryYear")))
         cardExpYr.send_keys(data["testexpyear"])
-        # print("EXPIRY YEAR ENTERED")
         # cvv
         cvv = WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.NAME, "securityCode")))
         cvv.send_keys(data["testcvv"])
@@ -127,11 +100,33 @@ while not in_stock:
         # SEND THE DOLLA
         WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, "//*[@id='submitButton']"))).click()
         print("CHECKING OUT...")
+        
+        try:
+            WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.XPATH, "//div[contains(text(),'Card')]")))
+            print("COULD NOT CHECKOUT")
+            refresh()
+        except:
+            checked_out = True
     except:
-        if not in_stock:
-            print("OUT OF STOCK")
-            time.sleep(5)
-            restart()
-        else:
-            print("EXCEPTION")
-            restart()
+        print("EXCEPTION")
+        refresh()
+
+while not checked_out:
+    try:
+        # click off of cookie notice
+        WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='onetrust-accept-btn-handler']"))).click()
+        add_to_basket = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.XPATH, "//*[@id='product-actions']/div[4]/div[1]/button")))
+        # code won't get past here if it's out of stock
+        print('IN STOCK')
+        add_to_basket.click()
+        # let basket update
+        time.sleep(2)
+        
+        try_to_checkout()
+    except:
+        print("OUT OF STOCK")
+        time.sleep(1)
+        refresh()
+
+print("BINGO.")
+driver.quit()
